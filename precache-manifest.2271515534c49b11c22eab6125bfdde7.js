3cec1e6a4acf956116d172bc2bdb919a56962448
jaithri21 <jaithri21@gmail.com>
2024-07-02 11:22:18 +0530

    Updates

---
+++ precache-manifest.2271515534c49b11c22eab6125bfdde7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd4d76095ae739cc5dafac600e4223fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fd8bb5b70a4a8801422",
+    "url": "/static/css/main.dd734ade.chunk.css"
+  },
+  {
+    "revision": "b436a2766ce63f9c59ed",
+    "url": "/static/js/2.9c29abba.chunk.js"
+  },
+  {
+    "revision": "7fd8bb5b70a4a8801422",
+    "url": "/static/js/main.a43df0ca.chunk.js"
+  },
+  {
+    "revision": "2b39c98d96c9238cdd2e",
+    "url": "/static/js/runtime-main.28f67ebe.js"
+  }
+]);
